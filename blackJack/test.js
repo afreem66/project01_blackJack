af6003459805613e4abe95d9a61c95f20c83a45f
afreem66 <afreem66@gmail.com>
2015-09-25 12:14:49 -0400

    created a way to update the bank depending on differernt outcomes and assuming blackjack pays 1.5 times bet.

--- blackJack/test.js
+++ blackJack/test.js
@@ -2,11 +2,13 @@
 var dealButton = $('.deal'),
     hitButton = $('.hit')
     stayButton = $('.stay');
+    bankRoll = 500
 ///this button deals cards to the players hands and looks for blackjack
 dealButton.on("click", function (e) {
   deal(thePlayer, theDealer);
   console.log(thePlayer.value());
   console.log(theDealer.value());
+  bankRoll -= $("#player-bet").val();
 
   checkBlackJack(thePlayer.value(), theDealer.value());
 })
@@ -15,6 +17,8 @@ hitButton.on("click", function(e) {
   thePlayer.add(deck[Math.floor(Math.random()*deck.length)])
     if(thePlayer.value() > 21) {
       alert("BUST!");
+      bankRoll -= $("#player-bet").val();
+
     } else {
       checkBlackJack(thePlayer.value(), theDealer.value());
     }
@@ -24,6 +28,12 @@ stayButton.on("click", function(e) {
   console.log(thePlayer.value());
   dealerRules(theDealer.value());
   console.log(theDealer.value());
+  findWinner(thePlayer.value(), theDealer.value());
+
+  reset(thePlayer, theDealer);
+  console.log(thePlayer.hand);
+  console.log(theDealer.hand);
+
 })
 
 
@@ -110,16 +120,6 @@ function Player (name) {
     }
 }
 
-///This Player method clears both hands and sends cards back to the deck array
-
-  this.reset = function () {
-
-    hand.aces.splice();
-    hand.nonAces.splice();
-
-}
-
-
 
 ///This section deals the original two cards to the player and dealer.
 
@@ -139,58 +139,77 @@ function deal(player, dealer) {
 function checkBlackJack(playerScore, dealerScore) {
   if (playerScore == 21 && dealerScore == 21) {
     console.log("Both, have blackjack it is a push");
+    bankRoll += $("#player-bet").val();
+;
+
   } else if (playerScore == 21) {
     console.log("BLACKJACK! " + playerName + " wins!")
+    bankRoll += ($("#player-bet").val()*2.5);
+
   } else if (dealerScore == 21) {
     console.log("BLACKJACK! Dealer wins");
+    bankRoll = bankRoll;
+
   } else {
     console.log(playerName + ", you have " + playerScore + ". Do you want to hit or stay?")
   }
 }
+
 ///this function lays out the rules for the dealer to stay and hit
-///Does it need to be a function or variable??? can I attach to constructor object?
 
 function dealerRules(dealerScore) {
-    if (dealerScore > 17) {
+    if (dealerScore >= 17) {
       console.log("The dealer has " + dealerScore + " and has to stay")
     } else {
-    while (dealerScore <= 17) {
+    while (dealerScore < 17) {
       console.log("The dealer has " + dealerScore + " and has to hit");
       theDealer.add(deck[Math.floor(Math.random()*deck.length)]);
-      console.log("The dealer now has " + dealerScore);
       return dealerScore;
       }
+
     }
 }
 
+///This function contains the logic that determines the winner
+
 function findWinner(playerScore, dealerScore) {
-  if (playerScore == dealerScore) {
+  if (dealerScore > 21) {
+    console.log("The dealer busts you win!");
+    bankRoll += ($("#player-bet").val()*2);
+
+  }
+    else if (playerScore > dealerScore) {
     console.log(playerName + " wins!");
+    bankRoll += ($("#player-bet").val()*2);
+
   } else if (playerScore < dealerScore) {
     console.log("BOOOO the dealer wins");
+    bankRoll = bankRoll;
+
   } else {
-    console.log("It is a push, you get your maoney back " + playerName);
+    console.log("It is a push, you get your money back " + playerName);
+    bankRoll += ($("#player-bet").val());
+
   }
+  console.log(bankRoll);
 }
-/// Here is where I am trying to create the players and their hands.
+
+///This function clears both hands and sends cards back to the deck array
+
+  function reset (player, dealer) {
+    if (player.hand.aces.length > 0) {
+      player.hand.aces.splice(0, player.hand.aces.length);
+    }
+    player.hand.nonAces.splice(0, player.hand.nonAces.length);
+
+    if (dealer.hand.aces.length > 0) {
+      dealer.hand.aces.splice(0, dealer.hand.aces.length);
+    }
+    dealer.hand.nonAces.splice(0, dealer.hand.nonAces.length);
+
+}
+
+/// Here is where I am trying to create the players.
 playerName = prompt("Do you want to play BlackJack? What is your name?");
 var thePlayer = new Player(playerName);
 var theDealer = new Player('Dealer');
-
-function game() {
-  deal(thePlayer, theDealer);
-  console.log(thePlayer.value());
-  console.log(theDealer.value());
-
-  dealerRules(theDealer.value());
-  checkWinner(thePlayer.value(), theDealer.value());
-
-  thePlayer.reset();
-  theDealer.reset();
-  for (var i = 0; i < dealt.length; i++) {
-    deck.push(dealt[i]);
-  }
-}
-
-///Whenever I add this section back in it just adds 4 cards to thePlayer
-///not the Dealer
